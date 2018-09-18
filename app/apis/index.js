03692f1f7529cb45a8ecb1d2100198ef023c1dee
akshay gupta <akshaygupta@akshays-Mac-mini.local>
2018-09-18 21:24:20 +0530

    Handled API failed error.
    - Added support to show alert if user exceeded search limit
      while searching for users.

--- app/apis/index.js
+++ app/apis/index.js
@@ -1,3 +1,5 @@
+import { Alert } from 'react-native';
+
 import { saveUsersDataToStore, saveRepositoryDetailsDataToStore } from "../actions";
 import { GITHUB_USER_SEARCH_API, DEFAULT_SEARCH_KEY } from "../constants/apis";
 
@@ -8,7 +10,12 @@ export const fetchAllGithubUsers = (dispatch, searchString = DEFAULT_SEARCH_KEY)
     const apiUrl = appendQueryParameter(GITHUB_USER_SEARCH_API, searchString);
     fetch(apiUrl)
     .then((response) => {
-        saveUsersDataToStore(JSON.parse(response._bodyInit), dispatch);
+        const data = JSON.parse(response._bodyInit);
+        if (data.message) {
+            Alert.alert("API error", data.message);
+        } else {
+            saveUsersDataToStore(data, dispatch);
+        }
     })
     // Saving this to console for now, just to make sure 
     // API is working.
